8bc9d5f90ef4f7df23a307e1e4bd2f2f61c5a333
Ivan <onlyformywork2033@gmail.com>
2021-06-02 20:51:30 +0300

    Create models, Media urls, register models.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products_img', blank=True)
+    short_description = models.CharField(max_length=128, blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
