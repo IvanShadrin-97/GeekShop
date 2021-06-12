f3b827bd87dba6ddc4119be8383c2caef819d632
Ivan <onlyformywork2033@gmail.com>
2021-06-12 11:53:43 +0300

    Added remove views and logic of total_quantity and total_price, but them do not display.

--- baskets/models.py
+++ baskets/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from users.models import User
 from products.models import Products
 
+
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
@@ -11,3 +12,18 @@ class Basket(models.Model):
 
     def __str__(self):
         return f'Корзина для {self.user.username} |  Продукт для {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
+
+    def total_price(self):
+        baskets = Basket.objects.filter(user=self.user)
+        for basket in baskets:
+            sum(basket.sum())
+        return baskets
+
+    def total_quantity(self):
+        baskets = Basket.objects.filter(user=self.user)
+        for basket in baskets:
+            sum(basket.quantity)
+        return baskets
