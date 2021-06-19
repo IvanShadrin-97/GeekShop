826a2e77b20da1e2f97aff9f532b34360da5821b
Ivan <onlyformywork2033@gmail.com>
2021-06-19 19:43:04 +0300

    Done With CRUD

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -25,8 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('users/', include('users.urls', namespace='users')),
-    path('products/', include('products.urls' , namespace='products')),
-    path('baskets/', include('baskets.urls' , namespace='baskets')),
+    path('products/', include('products.urls', namespace='products')),
+    path('baskets/', include('baskets.urls', namespace='baskets')),
+    path('admin-staf/', include('admins.urls', namespace='admins')),
+
 ]
 
 if settings.DEBUG:
